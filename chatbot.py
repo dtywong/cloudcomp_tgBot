0540c9489ba933ba0ac6917c3d330e890855923f
Wong Tin Yau David <tin-yau-david.wong@connect.polyu.hk>
2025-02-28 03:21:02 +0800

    Modify ChatBot to integrate  Redis cloud services and equip with ChatGPT;

--- chatbot.py
+++ chatbot.py
@@ -1,11 +1,13 @@
-## this file is based on version 13.7 of python telegram chatbot
-## and version 1.26.18 of urllib3
-## chatbot.oy
-import telegram
-from telegram.ext import Updater, MessageHandler, Filters
-# The messageHandler is used for all ressage updates
+from telegram import Update
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
+                          CallbackContext)
+from ChatGPT_HKBU import HKBU_ChatGPT
 import configparser
 import logging
+import redis
+
+global redis1
+
 
 def main():
     # Load your token and create an Updater for your Bot
@@ -13,25 +15,76 @@ def main():
     config.read('config.ini')
     updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
     dispatcher = updater.dispatcher
-    # You car set this loggirg nocule,
-    # so you will <row wher and why tnings da not work as expected
-    logging. basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                         level=logging. INFO)
+    global redis1
+    redis1 = redis.Redis(host=(config['REDIS']['HOST']),
+                         password=(config['REDIS']['PASSWORD']),
+                         port=(config['REDIS']['REDISPORT']),
+                         decode_responses=(config['REDIS']['DECODE_RESPONSE']),
+                         username=(config['REDIS']['USER_NAME']))
 
-    # register a dispatcher to handle message:
-    # here we register an echo dispatcher
-    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-    dispatcher. add_handler(echo_handler)
+    # You can set this logging module, so you will know when
+    # and why things do not work as expected Meanwhile, update your config.ini as:
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        level=logging.INFO)
 
-    # To start tne bot:
-    updater .start_polling()
-    updater. idle()
+    # register a dispatcher to handle message: here we register an echo dispatcher
+    # echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+    # dispatcher.add_handler(echo_handler)
+
+    # register a dispatcher to handle message: here we register an echo dispatcher
+    # echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+    # dispatcher.add_handler(echo_handler)
+    # dispatcher for chatgpt
+    global chatgpt
+    chatgpt = HKBU_ChatGPT(config)
+    chatgpt_handler = MessageHandler(Filters.text & (~Filters.command), equiped_chatgpt)
+    dispatcher.add_handler(chatgpt_handler)
+
+    # on different commands - answer in Telegram
+    dispatcher.add_handler(CommandHandler("add", add))
+    dispatcher.add_handler(CommandHandler("help", help_command))
+
+    # To start the bot:
+    updater.start_polling()
+    updater.idle()
+
 
 def echo(update, context):
-    reply_message = update.message. text.upper()
-    logging.info("“Update: " + str(update))
-    logging.info("“context: " + str(context))
-    context. bot.send_message(chat_id=update.effective_chat.id, text= reply_message)
+    reply_message = update.message.text.upper()
+    logging.info("Update: " + str(update))
+    logging.info("context: " + str(context))
+    context.bot.send_message(chat_id=update.effective_chat.id, text=reply_message)
+    # Define a few command handlers. These usually take the two arguments update and
+    # context. Error handlers also receive the raised TelegramError object in error.
+
+
+def help_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Helping you helping you.')
+
+
+def add(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /add is issued."""
+    try:
+        global redis1
+        logging.info(context.args[0])
+        msg = context.args[0]  # /add keyword <-- this should store the keyword
+        redis1.incr(msg)
+
+        # Get the count (already a string)
+        count = redis1.get(msg) or "0"
+        update.message.reply_text(f'You have said {msg} for {count} times.')
+
+    except (IndexError, ValueError):
+        update.message.reply_text('Usage: /add <keyword>')
+
+def equiped_chatgpt(update, context):
+        global chatgpt
+        reply_message = chatgpt.submit(update.message.text)
+        logging.info("Update: " + str(update))
+        logging.info("context: " + str(context))
+        context.bot.send_message(chat_id=update.effective_chat.id, text=reply_message)
+
 
 if __name__ == '__main__':
     main()
